79685cd5d53746bf0f9c4f895d32f5fd6a868747
Paul Standley <paulstandley1972@gmail.com>
2019-05-05 18:25:24 +0100

    HTTPS

---
+++ README.md
@@ -0,0 +1,14 @@
+# HTTP
+
+## **Traversy Media** [YouTube](https://www.youtube.com/watch?v=iYM2zFP3Zn0&t=1400s)
+
+---
+
+## start
+
+```BASH
+npm intall -g nodemon
+nodemon ./server.js localhost 5000
+```
+
+## [Download Postman](https://www.getpostman.com/downloads/)
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello from Express</h1>');
+  //res.send(req.header('host'));
+  res.send(req.header('user-agent'));
+});
+
+app.listen(5000, () => console.log('Server started on 5000'));
